7d0d44ae0d56f7a64ca580ccfb1f53e4fb24f8ab
Anton Berezin <tobez@tobez.org>
2012-05-16 16:25:39 +0200

    Real code.

--- lib/Net/SNMP/QueryEngine/AnyEvent.pm
+++ lib/Net/SNMP/QueryEngine/AnyEvent.pm
@@ -4,50 +4,182 @@ use 5.006;
 use strict;
 use warnings;
 
+our $VERSION = '0.01';
+
+use AnyEvent::Handle;
+use base 'AnyEvent::Handle';
+use Data::MessagePack;
+
+use constant RT_SETOPT   => 1;
+use constant RT_INFO     => 3;
+use constant RT_GET      => 4;
+use constant RT_GETTABLE => 5;
+use constant RT_REPLY    => 0x10;
+use constant RT_ERROR    => 0x20;
+
+sub read_handle;
+
+sub new
+{
+	my $class_or_ref = shift;
+	my %args = (connect => ["127.0.0.1", 7667], @_, on_read => \&read_handler);
+	my $self = $class_or_ref->SUPER::new(%args);
+	$self->{sqe}{condvar} = AnyEvent->condvar;
+	$self->{sqe}{pending} = 0;
+	$self->{sqe}{mp} = Data::MessagePack->new->prefer_integer;
+	$self->{sqe}{up} = Data::MessagePack::Unpacker->new;
+	$self->{sqe}{nup} = 0;
+	$self->{sqe}{cid} = int rand 1000000;
+	$self->{sqe}{cb} = {};
+	return $self;
+}
+
+sub wait
+{
+	my $self = shift;
+	return unless $self->{sqe}{pending};
+	$self->{sqe}{condvar}->recv;
+}
+
+sub cmd
+{
+	my ($self, $cb, @cmd) = @_;
+	$self->{sqe}{cb}{$self->{sqe}{cid}} = $cb;
+	$self->{sqe}{pending}++;
+	if ($self->{sqe}{pending} == 1 && $self->{sqe}{condvar}->ready) {
+		# XXX "reset" a condvar so "wait" can be correctly called again
+		$self->{sqe}{condvar} = AnyEvent->condvar;
+	}
+	$self->push_write($self->{sqe}{mp}->pack(\@cmd));
+}
+
+sub read_handler
+{
+	my $self = shift;
+	my $cl = length($self->{rbuf});
+	$self->{sqe}{nup} += $cl;
+again:
+	my $o = $self->{sqe}{up}->execute($self->{rbuf}, 0);
+	if ($o) {
+		my $data = $self->{sqe}{up}->data;
+		if (ref($data) ne "ARRAY" || @$data < 3 || !$self->{sqe}{cb}{$data->[1]}) {
+		} else {
+			$self->{sqe}{cb}{$data->[1]}->($self, $data->[0] & RT_REPLY, $data->[2]);
+			delete $self->{sqe}{cb}{$data->[1]};
+			$self->{sqe}{pending}--;
+			if ($self->{sqe}{pending} <= 0) {
+				$self->{sqe}{condvar}->send;
+			}
+		}
+		substr($self->{rbuf}, 0, $o - ($self->{sqe}{nup}-$cl), "");
+		$self->{sqe}{up}->reset;
+		$cl = $self->{sqe}{nup} = length($self->{rbuf});
+		goto again if $cl;
+	}
+	$self->{rbuf} = "";
+}
+
+sub setopt
+{
+	my ($self, $host, $port, $opts, $cb) = @_;
+	$self->cmd($cb, RT_SETOPT, ++$self->{sqe}{cid}, $host, $port, $opts);
+}
+
+sub get
+{
+	my ($self, $host, $port, $oids, $cb) = @_;
+	$self->cmd($cb, RT_GET, ++$self->{sqe}{cid}, $host, $port, $oids);
+}
+
+sub gettable
+{
+	my ($self, $host, $port, $oid, $max_rep, $cb) = @_;
+	if ($cb) {
+		$self->cmd($cb, RT_GETTABLE, ++$self->{sqe}{cid}, $host, $port, $oid, $max_rep);
+	} else {
+		$self->cmd($max_rep, RT_GETTABLE, ++$self->{sqe}{cid}, $host, $port, $oid);
+	}
+}
+
+sub info
+{
+	my ($self, $cb) = @_;
+	$self->cmd($cb, RT_INFO, ++$self->{sqe}{cid});
+}
+
 =head1 NAME
 
-Net::SNMP::QueryEngine::AnyEvent - The great new Net::SNMP::QueryEngine::AnyEvent!
+Net::SNMP::QueryEngine::AnyEvent - multiplexing SNMP query engine client using AnyEvent
 
 =head1 VERSION
 
 Version 0.01
 
-=cut
-
-our $VERSION = '0.01';
-
-
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+This is an AnyEvent-flavored Perl client for snmp-query-engine,
+a multiplexing SNMP query engine.
 
     use Net::SNMP::QueryEngine::AnyEvent;
 
-    my $foo = Net::SNMP::QueryEngine::AnyEvent->new();
-    ...
+    my $sqe = Net::SNMP::QueryEngine::AnyEvent->new;
 
-=head1 EXPORT
+    $sqe->setopt("127.0.0.1", 161, { community => "meow" }, sub {});
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    $sqe->gettable("127.0.0.1", 161, "1.3.6.1.2.1.1", sub {
+      my ($h, $ok, $r) = @_;
+      for my $t (@$r) {
+        print "$t->[0] => $t->[1]\n";
+      }
+    });
 
-=head1 SUBROUTINES/METHODS
+    $sqe->get("127.0.0.1", 161,
+      ["1.3.6.1.2.1.1.5.0", "1.3.6.1.2.1.25.1.1.0"],
+      sub {
+        my ($h, $ok, $r) = @_;
+        print "Hostname: $r->[0][1]\n";
+        print "Uptime  : $r->[1][1]\n";
+    });
 
-=head2 function1
+    $sqe->wait;
 
-=cut
+=head1 METHODS
 
-sub function1 {
-}
+=head2 new
 
-=head2 function2
+Constructor.  Takes the same arguments as the constructor of
+the base class, AnyEvent::Handle::new,
+but always overrides "on_read" callback.
 
-=cut
+By default, connects to snmp-query-engine listening on
+localhost, port 7667.  Override this by specifying
+a "connect" argument.
 
-sub function2 {
-}
+=head2 wait
+
+Enters event loop until there are no unanswered queries.
+Can be called multiple times.
+
+=head2 setopt
+
+Performs setopt request.
+
+=head2 getopt
+
+Performs getopt request.
+
+=head2 get
+
+Performs get request for arbitrary number
+of OIDs.
+
+=head2 gettable
+
+Performs gettable request.
+
+=head2 info
+
+Performs info request.
 
 =head1 AUTHOR
 
@@ -97,40 +229,32 @@ L<http://search.cpan.org/dist/Net-SNMP-QueryEngine-AnyEvent/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2012 Anton Berezin.
-
-This program is distributed under the (Revised) BSD License:
-L<http://www.opensource.org/licenses/bsd-license.php>
+Copyright (c) 2012, Anton Berezin "<tobez@tobez.org>". All rights
+reserved.
 
 Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
+modification, are permitted provided that the following conditions are
+met:
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
 
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
 
-* Neither the name of Anton Berezin's Organization
-nor the names of its contributors may be used to endorse or promote
-products derived from this software without specific prior written
-permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS ``AS IS'' AND ANY
+EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =cut
 
-1; # End of Net::SNMP::QueryEngine::AnyEvent
+1;
